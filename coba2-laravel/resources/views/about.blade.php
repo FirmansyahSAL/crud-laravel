c8341bda624fa2c970cd64ed615e58027d9a4c72
Firmansyah SAL <syah.firman55@gmail.com>
2022-07-13 14:22:48 +0700

    layouting template

--- coba2-laravel/resources/views/about.blade.php
+++ coba2-laravel/resources/views/about.blade.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SMA Blog | About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
+@extends('layouts.main')
+@section('container')
     <h1>Halaman About</h1>
     <h1>{{$name}}</h1>
     <p>{{ $email }}</p>
     <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
-    <script src="js/script.js"></script>
-</body>
-
-</html>
+  @endsection
