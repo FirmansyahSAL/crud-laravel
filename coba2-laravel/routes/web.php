ffcbf94dc37c2b3676ce31a805f6270c2b1bf7b7
Firmansyah SAL <syah.firman55@gmail.com>
2022-07-13 14:27:01 +0700

    update title

--- coba2-laravel/routes/web.php
+++ coba2-laravel/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "about",
         "name" => "aksa",
         "email" => "syah.firman55@gmail.com",
         "image" => "sma.jpg"
@@ -26,6 +29,8 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title" => "posts"
+    ]);
 });
 
