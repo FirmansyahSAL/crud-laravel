2d611f4c291101871bc8cbdce675ed4002a243f5
Firmansyah SAL <syah.firman55@gmail.com>
2022-07-17 23:19:55 +0700

    modified routes

--- coba2-laravel/routes/web.php
+++ coba2-laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,32 +50,23 @@ Route::get('/blog', function () {
     ]);
 });
 
+Route::get('/posts', function () {
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => Post::all()
+    ]);
+});
+
+
+
 // halaman single posts
 Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Mia San Mia",
-            "slug" => "Bayern-Munchen-FC",
-            "author" => "FSLaksana",
-            "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tempore sit reiciendis suscipit fuga non, cum culpa totam. Illo quisquam dolorem perferendis! Voluptatibus ex tempora aut accusantium odio placeat reprehenderit perferendis, illum veritatis labore magni consequuntur aperiam nisi voluptatum ad laudantium nam itaque nihil animi aliquam, sint excepturi sunt eveniet. Veniam pariatur vero vel? A reprehenderit quas ipsa inventore dolores libero id ad ut saepe, atque animi, delectus illum similique deserunt fuga quibusdam velit, pariatur voluptates culpa aut omnis eum?"
-        ],
-        [
-            "title" => "You Never Walk Alone",
-            "slug" => "Liverpool-FC",
-            "author" => "aksana",
-            "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tempore sit reiciendis suscipit fuga non, cum culpa totam. Illo quisquam dolorem perferendis! Voluptatibus ex tempora aut accusantium odio placeat reprehenderit perferendis, illum veritatis labore magni consequuntur aperiam nisi voluptatum ad laudantium nam itaque nihil animi aliquam, sint excepturi sunt eveniet. Veniam pariatur vero vel? A reprehenderit quas ipsa inventore dolores libero id ad ut saepe, atque animi, delectus illum similique deserunt fuga quibusdam velit, pariatur voluptates culpa aut omnis eum?"
-        ],
-    ];
+   
 
-    $new_posts = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] == $slug) {
-            $new_posts = $post;
-        }
-    }
+ 
     return view('post', [
         "title" => " Single Posts",
-        "post" => $new_posts
+        "post" => Post::find($slug)
     ]);
 });
 
