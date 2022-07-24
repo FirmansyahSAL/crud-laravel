2926a7a103e9c73706a2ce367fec4e6b49eafa29
Firmansyah SAL <syah.firman55@gmail.com>
2022-07-24 19:34:01 +0700

    modified 0.1

--- example-app/app/Models/Post.php
+++ example-app/app/Models/Post.php
@@ -1,57 +1,38 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+namespace App\Models;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('home', [
-        "title" => "Home"
-    ]);
-});
 
-Route::get('/about', function () {
-    return view('about', [
-        "title" => "about",
-        "name" => "aksa",
-        "email" => "syah.firman55@gmail.com",
-        "image" => "sma.jpg"
-    ]);
-});
-
-Route::get('/blog', function () {
-    $blog_posts = [
+class Post
+{
+    private static $blog_posts =
+    [
         [
             "title" => "Mia San Mia",
             "slug" => "Bayern-Munchen-FC",
             "author" => "FSLaksana",
             "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tempore sit reiciendis suscipit fuga non, cum culpa totam. Illo quisquam dolorem perferendis! Voluptatibus ex tempora aut accusantium odio placeat reprehenderit perferendis, illum veritatis labore magni consequuntur aperiam nisi voluptatum ad laudantium nam itaque nihil animi aliquam, sint excepturi sunt eveniet. Veniam pariatur vero vel? A reprehenderit quas ipsa inventore dolores libero id ad ut saepe, atque animi, delectus illum similique deserunt fuga quibusdam velit, pariatur voluptates culpa aut omnis eum?"
+
         ],
         [
-            "title" => "You Never Walk Alone",
+            "title" => "You Never Walk Alone Alone",
             "slug" => "Liverpool-FC",
             "author" => "aksana",
             "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tempore sit reiciendis suscipit fuga non, cum culpa totam. Illo quisquam dolorem perferendis! Voluptatibus ex tempora aut accusantium odio placeat reprehenderit perferendis, illum veritatis labore magni consequuntur aperiam nisi voluptatum ad laudantium nam itaque nihil animi aliquam, sint excepturi sunt eveniet. Veniam pariatur vero vel? A reprehenderit quas ipsa inventore dolores libero id ad ut saepe, atque animi, delectus illum similique deserunt fuga quibusdam velit, pariatur voluptates culpa aut omnis eum?"
-        ],
+
+        ]
     ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
 
-Route::get('/posts', [PostController::class, 'index']);
+    public static function all()
+    {
+        return collect(self::$blog_posts);
+    }
 
+    public static function find($slug)
+    {
+        $posts = static::all();
 
-
-// halaman single posts
-Route::get('posts/{slug}', [PostController::class, 'show']);
+        return $posts->firstWhere('slug', $slug);
+    }
+}
---
+++ example-app/resources/views/posts.blade.php
@@ -0,0 +1,12 @@
+
+
+@extends('layouts.main')
+@section('container')
+    @foreach ($posts as $post)
+        <article class="mb-3">
+            <h1><a href="/posts/{{ $post["slug"] }}"> {{ $post["title"]  }}</a></h1>
+            <h5>by:{{ $post["author"] }}</h5>
+            <p>{{ $post["Body"] }}</p>
+        </article>
+    @endforeach
+@endsection 
